d3481161bb88d6e3c1be8ac28bf1c8ff25487d6a
Don3008 <don3008@yandex.ru>
2019-01-27 01:11:21 +0300

    Добавьте файлы проекта.

---
+++ InputRequest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, surname, patronymic;
+            Console.WriteLine("Введите свои фамилию, имя и отчество.");
+            Console.Write("Введите фамилию:");
+            surname = Console.ReadLine();
+            Console.Write("Введите имя:");
+            name = Console.ReadLine();
+            Console.Write("Введите отчество:");
+            patronymic = Console.ReadLine();
+            Console.WriteLine(surname + " " + name + " " + patronymic);
+        }
+    }
+}
